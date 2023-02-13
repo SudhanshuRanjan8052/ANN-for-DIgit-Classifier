b5de067993b7103a8c0790120085259f88ba7aa4
SUDHANSHU RANJAN <97516533+SudhanshuRanjan8052@users.noreply.github.com>
2023-02-13 12:00:36 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+from PIL import Image
+import numpy as np
+import joblib
+
+img = Image.open(r"filepath")
+
+data = list(img.getdata())
+for i in range(len(data)):
+    data[i]=255-data[i]
+
+input = np.array(data)/256
+model = joblib.load("model.sav")
+
+res = model.predict([input])
+print("The predicted value is: ",res)
